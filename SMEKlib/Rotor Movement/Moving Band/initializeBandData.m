abf2c5280feeacd8e433752c998c35cd88e57a18
Lehikoinen Antti <antti.lehikoinen@aalto.fi>
2017-05-16 14:12:46 +0300

    Fixed erroneous sector angle for full-cross-section problems.
    Signed-off-by: Lehikoinen Antti <antti.lehikoinen@aalto.fi>

--- SMEKlib/Rotor Movement/Moving Band/initializeBandData.m
+++ SMEKlib/Rotor Movement/Moving Band/initializeBandData.m
@@ -60,14 +60,10 @@ agAngles_rotor( agAngles_rotor<0 ) = agAngles_rotor(agAngles_rotor<0) + 2*pi;
 [~, agOrder_rotor] = sort( agAngles_rotor );
 agNodes_rotor = agNodes_rotor(agOrder_rotor);
 
-%sectorAngle = max(agAngles_rotor) - min(agAngles_rotor);
-
 if isfield(msh, 'symmetrySectors')
     % periodicity conditions --> adding virtual nodes on the ag-rotor
     % surface to encompass the entire cross-section  
     symm = msh.symmetrySectors;
-    
-    %TODO: sort nodes first!
     
     %adding virtual rotor surface nodes
     p_ag_virt = [msh.p(:,agNodes_global) zeros(2, (symm-1)*N_ag_r)];
@@ -97,16 +93,10 @@ if isfield(msh, 'symmetrySectors')
     agAngles_rotor = atan2( p_ag_virt(2,(N_ag_s+1):end), p_ag_virt(1,(N_ag_s+1):end) );
     agAngles_rotor( agAngles_rotor<0 ) = agAngles_rotor(agAngles_rotor<0) + 2*pi;
     [~, agOrder_rotor] = sort( agAngles_rotor );
-
-    %sectorAngle = max(agAngles_rotor) - min(agAngles_rotor)
+    
     sectorAngle = 2*pi;
 else
-    %sorting rotor and stator nodes
-    %agAngles_rotor = atan2( msh.p(2, agNodes_rotor), msh.p(1, agNodes_rotor) );
-    %agAngles_rotor( agAngles_rotor<0 ) = agAngles_rotor(agAngles_rotor<0) + 2*pi;
-    %[~, agOrder_rotor] = sort( agAngles_rotor );
-
-    sectorAngle = max(agAngles_rotor) - min(agAngles_rotor);
+    sectorAngle = 2*pi;
 end
 
 agAngles_stator = atan2( msh.p(2, agNodes_stator), msh.p(1, agNodes_stator) );
@@ -116,35 +106,16 @@ agAngles_stator( agAngles_stator<0 ) = agAngles_stator(agAngles_stator<0) + 2*pi
 agAngles_all = [agAngles_stator agAngles_rotor];
 
 %linear indices to rotor-surface nodes in t_ag
-%inds_r = find( ismember(t_ag(:), agNodes_rotor ) );
 inds_r = find( ismember(agNodes_global(t_ag(:)), agNodes_rotor ) );
 
 %rotor nodes in sorted order; and the indices of the original air-gap
 %triangulation in that list
 temp = (N_ag_s + 1):(N_ag_s + N_ag_r); sortedNodes_rotor = temp(agOrder_rotor);
-%sortedNodes_rotor = agNodes_rotor(agOrder_rotor);
 
 [~, originalPositions_rotor] = ismember( t_ag(inds_r), sortedNodes_rotor );
 
-%{
-el_rotorBased = find( sum(ismember(t_ag, agNodes_rotor),1) == 2);
-%finding rotor-based elements (= long edge on the rotor surface)
-el_rotorBased = find( sum(ismember(t_ag, agNodes_rotor),1) == 2);
-
-%sorting rotor elements
-tempAngles = atan2( 0.5*(msh.p(2,t_ag(1,el_rotorBased))+msh.p(2,t_ag(3,el_rotorBased))), 0.5*(msh.p(1,t_ag(1,el_rotorBased))+msh.p(1,t_ag(3,el_rotorBased))) );
-[~,rotorElementOrder] = sort( tempAngles );
-el_rotorBased = el_rotorBased( rotorElementOrder );
-
-%re-sorting?
-elementOrder = 1:Ne_ag; Ne_ag_r = numel(el_rotorBased);
-elementOrder(el_rotorBased) = el_rotorBased( mod((1:Ne_ag_r) - 1 - 0, Ne_ag_r) + 1 );
-t_ag = t_ag(:, elementOrder); t_ag = sort(t_ag, 1);
-%}
-
-
+%angle difference for changing the band mesh
 shiftTol = sectorAngle / N_ag_r;
-
 
 bandData = struct('agNodes_global', agNodes_global, 'agAngles_all', agAngles_all, ...
     'Ne_ag', Ne_ag, 'N_ag_s', N_ag_s, 'N_ag_r', N_ag_r, ...
@@ -160,59 +131,6 @@ if isfield(msh, 'symmetrySectors')
             virt_identities;
             msh.periodicityCoeff.^virt_sectors];
     
-end
-
-return;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% plotting several non-necessary demonstrations here
-
-plot(msh.p(1, agNodes_stator), msh.p(2, agNodes_stator), 'bo');
-plot(msh.p(1, agNodes_rotor), msh.p(2, agNodes_rotor), 'ro');
-
-%triplot(t_ag(:,el_rotorBased)', msh.p(1,:), msh.p(2,:), 'm')
-
-%plotting a demonstration
-figure(5); clf; hold on;
-p_ag = msh.p; 
-%plot( p_ag(1, t_ag(1,el_rotorBased)), p_ag(2, t_ag(1,el_rotorBased)), 'ro-')
-
-plotmesh = msh; plotmesh.bandData = bandData;
-
-
-Np = 20;
-angles = linspace(0, 2.5, Np)/180*pi;
-[~,n_plot] = min( abs(agAngles_rotor ) );
-for kp = 1:Np
-    rotorAngle = angles(kp);
-    
-    [t_ag_new, ~, p_ag] = updateRotorPosition(rotorAngle, plotmesh);
-    t_ag_new = reshape( agNodes_global(t_ag_new(:)), 3, []);
-    
-    %{
-    p_ag(:,rotorNodes_all) = [cos(rotorAngle) -sin(rotorAngle); sin(rotorAngle) cos(rotorAngle)] * msh.p(:,rotorNodes_all);
-    %t_ag = updateRotorPosition(angles(kp), msh, bandData);
-
-    nodeShift = -floor( (rotorAngle - 1*shiftTol/2) / shiftTol ) - 1;
-    %newOrder = bandData.agOrder_rotor( mod((1:bandData.N_ag_r)-1 + nodeShift, bandData.N_ag_r) + 1 );
-    newPositions = mod(originalPositions_rotor - 1 + nodeShift, N_ag_r ) + 1;
-    
-    %newPositions = agNodes_rotor( agOrder_rotor(newPositions) );
-    %newPositions = sortedNodes_rotor(newPositions);
-    
-    t_ag_new = t_ag;
-    %t_ag(inds_r) = agNodes_rotor(inds_agOrder_r(newPositions));
-    t_ag_new(inds_r) = sortedNodes_rotor(newPositions);
-    t_ag_new = reshape( agNodes_global(t_ag_new(:)), 3, []);
-    %}
-    
-    figure(5); clf; hold on;
-    triplot(t_ag_new', p_ag(1,:), p_ag(2,:), 'm');
-    plot(p_ag(1, sortedNodes_rotor), p_ag(2,sortedNodes_rotor), 'ko-');
-    plot(p_ag(1, agNodes_rotor(n_plot)), p_ag(2,agNodes_rotor(n_plot)), 'mx');
-    axis([0.07 0.101 -0.02 0.02]);
-    
-    pause(0.2);
 end
 
 

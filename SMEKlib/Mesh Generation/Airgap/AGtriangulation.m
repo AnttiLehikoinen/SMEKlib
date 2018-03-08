5040c43927bf1492a2cb4848317cbf81e7912ef0
AnttiLehikoinen <antti.lehikoinen@alumni.aalto.fi>
2018-03-08 07:22:19 +0200

    Removed debug messages.

--- SMEKlib/Mesh Generation/Airgap/AGtriangulation.m
+++ SMEKlib/Mesh Generation/Airgap/AGtriangulation.m
@@ -42,7 +42,6 @@ classdef AGtriangulation < handle
         end
         %}
         function [tag_local, p, tag_global] = t_ag(this, varargin)
-            disp('wfsd')
             if numel(varargin)
                 rotorAngle = varargin{1};
             else
@@ -67,15 +66,17 @@ classdef AGtriangulation < handle
             if ~isfield(this.misc, 'p_orig')
                 %called for the first time --> initializing info
                 this.misc.p_orig = this.p_virt;
-                this.misc.agAngles_all = atan2( this.p_virt(2,:), this.p_virt(1,:) );
+                %this.misc.agAngles_all = atan2( this.p_virt(2,:),
+                %this.p_virt(1,:) ); %not needed, it seems
                 this.misc.nr = setdiff(this.n_moving, this.n_bnd); %rotor boundary nodes
             end
             a = atan2(x(2), x(1)); %angle of the displacement
             nmid = this.n_bnd;
             %this.p_virt(:, nmid) = this.misc.p_orig(:,nmid) + ...
             %    r*[cos(this.misc.agAngles_all(nmid)-a); sin(this.misc.agAngles_all(nmid)-a)];
-            this.p_virt(:, nmid) = this.misc.p_orig(:,nmid) + ...
-                bsxfun(@times, x/2, abs(cos(this.misc.agAngles_all(nmid)-a)));
+            %this.p_virt(:, nmid) = this.misc.p_orig(:,nmid) + ...
+            %    bsxfun(@times, x/2, abs(cos(this.misc.agAngles_all(nmid)-a)));
+            this.p_virt(:, nmid) = bsxfun(@plus, this.misc.p_orig(:,nmid), x/2);
             nmid = this.misc.nr;
             this.p_virt(:, nmid) = bsxfun(@plus, this.misc.p_orig(:,nmid), x);
         end

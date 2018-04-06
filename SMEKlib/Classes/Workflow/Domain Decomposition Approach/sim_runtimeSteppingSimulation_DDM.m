bae9bbe5990de211650bd9085c19373f2ba7a96b
AnttiLehikoinen <antti.lehikoinen@alumni.aalto.fi>
2018-04-06 16:52:10 +0300

    DDM WIP.

--- SMEKlib/Classes/Workflow/Domain Decomposition Approach/sim_runtimeSteppingSimulation_DDM.m
+++ SMEKlib/Classes/Workflow/Domain Decomposition Approach/sim_runtimeSteppingSimulation_DDM.m
@@ -3,6 +3,12 @@ function sim = sim_runtimeSteppingSimulation_DDM(sim, pars, varargin)
 %
 % (c) 2018 Antti Lehikoinen / Aalto University
 
+%TODO
+% [ ] CN initial conditions
+% [ ] remove quick fixes from e.g. voltage
+% [ ] remove unnecessary data from impulse response computation
+% [ ] switch to harmonic reluctivity? (maybe not required yet
+
 %basic setup
 f = pars.f;
 w = 2*pi*f;
@@ -62,10 +68,13 @@ tempcell_SDD = cell(1, Qs_sector); [tempcell_SDD{:}] = deal( -Hvar(1:ND, 1:ND) )
 tempcell_SDI = cell(1, Qs_sector); [tempcell_SDI{:}] = deal( -Hvar(1:ND, (ND+1):end) );
 tempcell_ID = cell(1, Qs_sector); [tempcell_ID{:}] = deal( Hvar((ND+1):end, 1:ND) );
 tempcell_II = cell(1, Qs_sector); [tempcell_II{:}] = deal( Hvar((ND+1):end, (ND+1):end) );
-Q_SDD = P_m2D'*blkdiag(tempcell_SDD{:})*P_m2D; %clear tempcell_SDD;
-Q_SDI = P_m2D'*blkdiag(tempcell_SDI{:})*L_s; %clear tempcell_SDI;
-Q_ID = L_s'*blkdiag(tempcell_ID{:})*P_m2D; %clear tempcell_ID;
-Q_II = L_s'*blkdiag(tempcell_II{:})*L_s; %clear tempcell_II;
+Q_SDD = P_m2D'*blkdiag(tempcell_SDD{:})*P_m2D; clear tempcell_SDD;
+Q_SDI = P_m2D'*blkdiag(tempcell_SDI{:})*L_s; clear tempcell_SDI;
+Q_ID = L_s'*blkdiag(tempcell_ID{:})*P_m2D; clear tempcell_ID;
+Q_II = L_s'*blkdiag(tempcell_II{:})*L_s; clear tempcell_II;
+
+%sim.misc.Sc = Sc;
+%sim.misc.Mtot = Mtot;
 
 Sc = Sc + [Q_SDD sparse(sim.Np, Nu) Q_SDI sparse(sim.Np, Ni_r);
     sparse(Nu, sim.Np + Nu + Ni_r + Ni_s);
@@ -90,16 +99,17 @@ end
 alpha2 = 2; %weight for implicit (k+1) step; 1 for CN, 2 for BE
 alpha1 = 2 - alpha2;
 
-
 %initializing previous residual term
 res_prev = zeros(size(Sc, 1), 1);
 
-for kt = 2:3%Nsamples
+for kt = 2:100%Nsamples
     disp(['Time step ' num2str(kt) '...']);
     
     S_ag = sim.msh.get_AGmatrix(wm*tsamples(kt), Ntot);
-    
+
     Qconst = S_ag + Sc + (2/alpha2)*Mtot;
+    
+    %sim.misc.Sag = S_ag;
     
     %load function
     Ustep = Ufun(tsamples(kt));
@@ -114,21 +124,22 @@ for kt = 2:3%Nsamples
     FD(indI) = FD(indI) - L_s' * reshape(hconst2((ND+1):end,:), [], 1);
     FL = FL + FD;
     
-    if kt == 2
-        sim.misc.hconst2 = hconst2;
-    end
+    %sim.misc.fh = [P_m2D*FD(indA); FD(indI)];
 
     
     Xsamples(:,kt) = Xsamples(:,kt-1); %initial condition for NR
-    for kiter = 1:5
+    for kiter = 1:50
         [J, res] = Jc.eval(Xsamples(:,kt), sim.nu_fun); 
         
         res_tot = PT'*(res + Qconst*Xsamples(:,kt) - FL - 0*(alpha1/alpha2)*res_prev);
         
+        %sim.misc.J = J;
+        %sim.misc.res = res;
+        
         resNorm = norm(res_tot) / norm(FL);
         disp(['    Newton step ' num2str(kiter) ', relative residual ' num2str(resNorm) '.']);
         %disp(['    Newton step ' num2str(kiter) ', relative residual ' num2str(resNorm) '.']); fflush(stdout); uncomment in Octave
-        if resNorm < 1e-9
+        if resNorm < 1e-6
             break;
         end
         
@@ -142,7 +153,12 @@ for kt = 2:3%Nsamples
         [zeros(Ntot-sim.results.Ni_s, 1); Ustep(1:sim.results.Ni_s)];
     
     %plotting currents
-    Is = Xsamples(indI(1:3), 1:kt);
-    figure(11); clf;
-    plot( Is'); drawnow;
+    %Is = Xsamples(indI(1:3), 1:kt);
+    %figure(11); clf;
+    %plot( Is'); drawnow;
+end
+
+sim.results.Xt = Xsamples;
+
+
 end

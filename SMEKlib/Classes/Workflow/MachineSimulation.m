b9647a23d651d3627408321e40f8fd7b5aceae0c
AnttiLehikoinen <antti.lehikoinen@alumni.aalto.fi>
2018-04-30 16:28:39 +0300

    DDM WIP.

--- SMEKlib/Classes/Workflow/MachineSimulation.m
+++ SMEKlib/Classes/Workflow/MachineSimulation.m
@@ -138,6 +138,16 @@ classdef MachineSimulation < handle
                 I = this.results.Xt(indI,:);
             end
         end
+        function I = Ish(this)
+            %Ish current from time-harmonic analysis
+            indI = this.Np + this.results.Nu_s + this.results.Nu_r + ...
+                (1:this.results.Ni_s);
+            Nvars = size(this.results.Xh, 1)/2;
+            I = this.results.Xh(indI) - 1i*this.results.Xh(Nvars+indI);
+            if this.dims.connection_stator == defs.star
+                I = [1 0;0 1;-1 -1] * I;
+            end
+        end
             
     end
 end

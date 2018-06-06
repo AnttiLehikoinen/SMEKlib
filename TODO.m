f3a0b742b2c5cef529bd47b3e1257637b6d0a104
AnttiLehikoinen <antti.lehikoinen@alumni.aalto.fi>
2018-06-06 11:54:54 +0300

    Updated todo.
    Signed-off-by: AnttiLehikoinen <antti.lehikoinen@alumni.aalto.fi>

--- TODO.m
+++ TODO.m
@@ -2,8 +2,9 @@
 [ ] Generalize gwrap's loadmesh for several element types, by scanning
 entire elements description into an array, and then chopping it into
 element types.
-[ ] Add possibility to use pre-defined triangulation with AirgapTriangulation.  
+[x] Add possibility to use pre-defined triangulation with AirgapTriangulation.  
       [ ] Add higher-order elements here.  
 [ ] Add higher-order elements to AGT in general.  
+[ ] Polynomial interpolation inside an AGT-child class--> better BEMF.
 [ ] Finish WST2 function.
 %}

4ec15206946fc2c8deef0611cf2008ce567307f9
AnttiLehikoinen <antti.lehikoinen@alumni.aalto.fi>
2018-07-04 10:42:41 +0300

    Static simulation updates. -Plotting fix. -Possible to run several different simulations in series.

--- SMEKlib/Classes/Workflow/sim_runStaticSimulation.m
+++ SMEKlib/Classes/Workflow/sim_runStaticSimulation.m
@@ -17,27 +17,37 @@ Jc = JacobianConstructor(sim.msh, Nodal2D(Operators.curl), Nodal2D(Operators.cur
 
 S_ag = sim.msh.get_AGmatrix(0, Ntot);
 Qconst = S_ag + Sc;
-FL = pars.U;
 
-Xs = zeros(Ntot, 1);
+N = size(pars.U, 2);
 
-for kiter = 1:50
-    [J, res] = Jc.eval(Xs, sim.nu_fun);
-    
-    res_tot = PT'*(res + Qconst*Xs - FL);
-    
-    resNorm = norm(res_tot) / norm(FL);
-    disp(['    Newton step ' num2str(kiter) ', relative residual ' num2str(resNorm) '.']);
-    %disp(['    Newton step ' num2str(kiter) ', relative residual ' num2str(resNorm) '.']); fflush(stdout); uncomment in Octave
-    if resNorm < 1e-6
-        break;
+FL = bsxfun(@plus, pars.U, sim.matrices.F);
+Xs_all = zeros(Ntot, N);
+
+for k = 1:N
+    if k == 1
+        Xs = zeros(Ntot, 1);
+    else
+        Xs = Xs_all(:,k-1);
     end
-    
-    Jtot = PT' * (J + Qconst) * PT;
-    
-    Xs = Xs - PT*( Jtot\res_tot );
+    for kiter = 1:50
+        [J, res] = Jc.eval(Xs, sim.nu_fun);
+
+        res_tot = PT'*(res + Qconst*Xs - FL(:,k));
+
+        resNorm = norm(res_tot) / norm(FL(:,k));
+        disp(['    Newton step ' num2str(kiter) ', relative residual ' num2str(resNorm) '.']);
+        %disp(['    Newton step ' num2str(kiter) ', relative residual ' num2str(resNorm) '.']); fflush(stdout); uncomment in Octave
+        if resNorm < 1e-6
+            break;
+        end
+
+        Jtot = PT' * (J + Qconst) * PT;
+
+        Xs = Xs - PT*( Jtot\res_tot );
+    end
+    Xs_all(:,k) = Xs;
 end
 
-sim.results.Xs = Xs;
+sim.results.Xs = Xs_all;
 
 end

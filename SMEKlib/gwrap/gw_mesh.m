dc301f6746a8016095b3fef0a3486ba546e7678b
AnttiLehikoinen <antti.lehikoinen@alumni.aalto.fi>
2018-09-13 17:27:52 +0300

    gmsh 4.0x compatibility quickfix.

--- SMEKlib/gwrap/gw_mesh.m
+++ SMEKlib/gwrap/gw_mesh.m
@@ -6,6 +6,6 @@ else
     filename = [gm.gpath 'gm_geo.geo'];
 end
 
-system(['"' gm.gpath '"' 'gmsh ' '"' filename '"' ' -2']);
-
+%system(['"' gm.gpath '"' 'gmsh ' '"' filename '"' ' -2']);
+system(['"' gm.gpath '"' 'gmsh ' '"' filename '"' ' -2 -format msh2'])
 end

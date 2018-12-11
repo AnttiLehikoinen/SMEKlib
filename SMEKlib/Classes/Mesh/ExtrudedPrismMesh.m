a26065c38d0edc183cd2c9cc2be75e7daad881be
AnttiLehikoinen <antti.lehikoinen@alumni.aalto.fi>
2018-12-11 11:37:03 +0200

    WIP.

--- SMEKlib/Classes/Mesh/ExtrudedPrismMesh.m
+++ SMEKlib/Classes/Mesh/ExtrudedPrismMesh.m
@@ -6,6 +6,7 @@ classdef ExtrudedPrismMesh < PrismMeshBase3D
     properties
         Nfaces_tri, Nfaces_square
         faces_tri, faces_square
+        Ne2, Nl
     end
     
     methods
@@ -31,6 +32,9 @@ classdef ExtrudedPrismMesh < PrismMeshBase3D
             msh3.elementType = Elements.prism;
             msh3.Nfaces_tri = Nfaces_tri;
             msh3.Nfaces_square = Nfaces_square;
+            
+            msh3.Ne2 = Ne2;
+            msh3.Nl = Nl;
             
             %setting nodes
             msh3.nodes = [repmat(msh2.p, 1, Nl);
@@ -131,6 +135,31 @@ classdef ExtrudedPrismMesh < PrismMeshBase3D
         
         function y = t(msh3)
             y = msh3.elements;
+        end
+        
+        function els = extrudeElements(msh3, els, varargin)
+            if numel(varargin)
+                layers = varargin{1};
+            else
+                layers = 1:(msh3.Nl-1);
+            end
+            els = kron(layers-1, msh3.Ne2*ones(1, numel(els))) + repmat(els, 1, numel(layers));            
+        end
+        
+        function [Bvec, h] = Bquiver(msh3, A, els, varargin)
+            if ~any(els) || els(1) < 0
+                els = 1:size(msh3.elements,2);
+            end
+            %plotting solution at element centers
+            xc = msh3.elementCenters(els);
+            Bvec = zeros(3, numel(els));
+            xref = [0.25; 0.25; 0.5];
+            Wcurl = Nedelec3DPrism(Operators.curl); %shape function
+            for k = 1:9
+                Bvec = Bvec + bsxfun(@times, Wcurl.eval(k, xref, msh3, els), A(abs(msh3.elements2edges(k,els)))' );
+                %Avec = Avec + Wcurl.eval(k, xref, msh, []);
+            end
+            h = quiver3(xc(1,:), xc(2,:), xc(3,:), Bvec(1,:), Bvec(2,:), Bvec(3,:), varargin{:});
         end
         
         function x0 = elementCenters(msh3, elem)
